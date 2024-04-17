766571fcaa07b084989e2730fdfb30d8470d90d1
mahamjabbar456 <mahamjabbar320@gmail.com>
2024-04-17 12:09:43 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt({
+    name: "sentence",
+    type: "input",
+    message: "Enter your sentence to count your word : "
+});
+const word = answer.sentence.trim().split(" ");
+console.log(word);
+console.log(`Your sentence word length is ${word.length}`);
